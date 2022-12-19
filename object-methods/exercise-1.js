846a3822232ce1ec560ef48534083aa4802a260d
marcoglp98 <marcoglp98@gmail.com>
2022-12-19 18:07:25 +0100

    add object methods exercise

---
+++ object-methods/exercise-1.js
@@ -0,0 +1,10 @@
+const person = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+let keys = Object.entries(person);
+console.log(keys);
+
+// Print values of person using Object.keys
