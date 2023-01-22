9717b4803f16ba5989a5b2de52bf47b9a121dd2b
marcoglp98 <marcoglp98@gmail.com>
2023-01-22 19:14:18 +0100

    add promise exercises

---
+++ promise/exercise.js
@@ -0,0 +1,13 @@
+const number = 15;
+
+let newPromise = new Promise ((resolve, reject) => {
+if (number > 10){
+    resolve(number);
+} else (reject(number));
+})
+
+newPromise
+.then ((val) => console.log(val))
+.catch ((err) => console.log(err))
+
+console.log(newPromise);
